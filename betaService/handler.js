7692b5699a00abb73b46c15ee88c9e3a44ef4a27
singularityhacker <singularityhacker@gmail.com>
2018-10-08 18:46:18 -0400

    Configurations
    Added app name and tenant and added dynamic html page content

--- betaService/handler.js
+++ betaService/handler.js
@@ -3,12 +3,20 @@
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'We are GO!'
-      //input: event,
-    }),
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: pageHTML
+    // body: JSON.stringify({
+    //   message: 'Lets do it!',
+    //   input: event,
+    // }),
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+let pageHTML = `<h1>hello ${5*2} friiends!</h1>`;
+
+
